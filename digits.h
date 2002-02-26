f9c0bd274ae2929181ca5463d797308e0babaf19
dse <dse@21bf9b18-a41b-0410-b0b2-96866cf41aed>
2002-02-26 01:33:22 +0000

    Initial revision
    git-svn-id: file:///var/lib/svn/dse/vtclock-trunk@1717 21bf9b18-a41b-0410-b0b2-96866cf41aed

---
+++ digits.h
@@ -0,0 +1,18 @@
+#define DIGIT_HEIGHT = 9
+#define DIGIT_WIDTH = 11
+
+char *digits[] = {
+
+"  .oooo.  \nd8P' `Y8b \n888    888\n888    888\n888    888\n`88b  d88'\n `Y8bd8P'",
+"     .o \n   o888 \n    888 \n    888 \n    888 \n    888 \n   o888o\n",
+"  .oooo.  \n.dP\"\"Y88b \n      ]8P'\n    .d8P' \n  .dP'    \n.oP     .o\n8888888888",
+"  .oooo.  \n.dP\"\"Y88b \n      ]8P'\n    <88b. \n     `88b.\no.   .88P \n`8bd88P'  ",
+"      .o  \n    .d88  \n  .d'888  \n.d'  888  \n88ooo888oo\n     888  \n    o888o ",
+"  oooooooo\n dP\"\"\"\"\"\"\"\nd88888b.  \n    `Y88b \n      ]88 \no.   .88P \n`8bd88P'  ",
+"    .ooo  \n  .88'    \n d88'     \nd888P\"Ybo.\nY88[   ]88\n`Y88   88P\n `88bod8' ",
+" ooooooooo\nd\"\"\"\"\"\"\"8'\n      .8' \n     .8'  \n    .8'   \n   .8'    \n  .8'     ",
+  " .ooooo.  \nd88'   `8.\nY88..  .8'\n `88888b. \n.8'  ``88b\n`8.   .88P\n `boood8' ",
+" .ooooo.  \n888' `Y88.\n888    888\n `Vbood888\n      888'\n    .88P' \n  .oP'    "
+};
+
+char *colon = "\n\n o8o\n `\"'\n o8o\n `\"'\n";
---
+++ vtclock.c
@@ -0,0 +1,95 @@
+/*
+ * vtclock.c
+ * 
+ * Program to display a large digital clock.
+ *
+ * Font "stolen" from figlet
+ *
+ * Original code by Rob Hoeft.
+ * Enhancements by Darren Embry.
+ *
+ */
+
+#include <ncurses.h>
+#include "digits.h"
+#include <time.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <time.h>
+
+void 
+mydelay() 
+     /* sleep until second changes.  does this by polling every
+      * 1/10 second.  close enough for government work. */
+{
+  static struct timeval timeout;
+  static time_t prevsecs = (time_t)0;
+  time_t secs;
+  while (1) {
+    time(&secs);
+    if (prevsecs != secs) {
+      prevsecs = secs;
+      return;
+    }
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 100000;	/* 0.1 secs */
+    select(0,NULL,NULL,NULL,&timeout);
+    prevsecs = secs;
+  }
+}
+
+int
+main() {
+  WINDOW *h1, *h2, *m1, *m2, *s1, *s2, *c1, *c2;
+  int startx, starty, di_width, cl_height, co_width, cl_width;
+  int i;
+  struct tm *tm_time;
+  time_t t_time;
+
+  initscr();
+
+  cl_height = 9;
+  di_width = 11;
+  co_width = 6;
+  cl_width = co_width * 2 + di_width * 6;
+  
+  starty = (LINES - cl_height) / 2;
+  startx = (COLS - cl_width) / 2;
+
+  h1 = newwin(cl_height, di_width, starty, startx); startx += di_width;
+  h2 = newwin(cl_height, di_width, starty, startx); startx += di_width;
+  c1 = newwin(cl_height, co_width, starty, startx); startx += co_width;
+  m1 = newwin(cl_height, di_width, starty, startx); startx += di_width;
+  m2 = newwin(cl_height, di_width, starty, startx); startx += di_width;
+  c2 = newwin(cl_height, co_width, starty, startx); startx += co_width;
+  s1 = newwin(cl_height, di_width, starty, startx); startx += di_width;
+  s2 = newwin(cl_height, di_width, starty, startx);
+
+  while (1) {
+    time(&t_time);
+    tm_time = localtime(&t_time);
+    mvwprintw(h1, 0, 0, digits[tm_time->tm_hour / 10]);
+    mvwprintw(h2, 0, 0, digits[tm_time->tm_hour % 10]);
+    mvwprintw(m1, 0, 0, digits[tm_time->tm_min / 10]);
+    mvwprintw(m2, 0, 0, digits[tm_time->tm_min % 10]);
+    mvwprintw(s1, 0, 0, digits[tm_time->tm_sec / 10]);
+    mvwprintw(s2, 0, 0, digits[tm_time->tm_sec % 10]);
+    mvwprintw(c1, 0, 0, colon);
+    mvwprintw(c2, 0, 0, colon); 
+    wrefresh(h1);
+    wrefresh(h2);
+    wrefresh(c1);
+    wrefresh(m1);
+    wrefresh(m2);
+    wrefresh(c2);
+    wrefresh(s1);
+    wrefresh(s2);
+    mydelay();
+  }
+ 
+  endwin();
+  return 0;
+}
+
